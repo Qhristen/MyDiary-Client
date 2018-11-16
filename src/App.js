72164e8833ef018f19d046b5ef6d01e8d3d78fc5
victor chibuike <victorblackshady@gmail.com>
2018-11-16 19:54:01 +0100

    chore(react setup): Craete an empty react project Install and configure webpack configure bable setup a dummy react app [finishes #160604387]

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Test My Diary app to make sure react works</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
