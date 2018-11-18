a7b9836a791d366cb463c820213d4f48b908d455
victor chibuike <victorblackshady@gmail.com>
2018-11-18 23:41:44 +0100

    chore(redux): Setup empty redux project - Setup empty redux folder structure - Add redux store to base app [Deliver #162038989]

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import store from './redux/store/index'
 
 // Routes
 import Routes from './components/Routes/Routes';
@@ -9,10 +9,11 @@ import Routes from './components/Routes/Routes';
 
 const App = () => {
   return (
-
+    <Provider store={store}>
         <Router>
           <Routes />
         </Router>
+      </Provider>
   );
 };
 
--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -3,15 +3,20 @@ import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 // Routes
-const  LandingPage = lazy(() => import('../../modules/public/LandingPage'));
-
-
+const LandingPage  = lazy(() => import('../../modules/public/LandingPage'));
+const Todos = lazy(() => import('../../components/todos/index'));
 const Routes = () => {
   return (
-      <Switch>
-         <h1> Welcome to my diary front-end</h1>
+    <Suspense fallback={<div>Loading...</div>}>
+    <Switch>
+         <Route
+         path="/" 
+         render={()=> {
+           return <Todos />;
+         }}
+       />
       </Switch>
-      
+    </Suspense>
   );
 };
 
